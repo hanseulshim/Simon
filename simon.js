4c373a0491f6d81e468dab56d518eeb7ecec4007
Hanseul Shim <hanseulshim@gmail.com>
2016-09-15 14:37:38 -0500

    updated code using objects

--- simon.js
+++ simon.js
@@ -1,51 +1,107 @@
-$(document).ready(function(){
-	
-	$('#top-left').on("click",function(){
-		alert("left");
-	});
-		$('#top-right').on("click",function(){
-		alert("right");
-	});
-			$('#bottom-left').on("click",function(){
-		alert("bottom-left");
-	});
-				$('#bottom-right').on("click",function(){
-		alert("bottom-right");
-	});
-	
-	
-$('#start').on("click",game);
+$(document).ready(function () {
+  simon.init()
+})
 
-});
+var simon = {
+  round: 0,
+  gameover: false,
+  sequence: [],
+  move: [],
+  result: false,
+  strict: false,
 
-function game(){
-	var round,rand,sequence,copy;
-	gameInit();
-	$('.roundNum').html(round);
+  init: function () {
+    var that = this
+    $('#start').on('click', () => that.startGame())
 
-	function gameInit(){
-		round=0;
-		rand=0;
-		sequence=[];
-		copy=[];
-	}
+    // Enables/Disables strict mode
+    $('#strict').on('click', () => {
+      that.strict = !that.strict
+      $('#strict').toggleClass('inactive')
+    })
+  },
 
-	function setSequence(){
-		rand = randomize;
-		sequence.push(rand);
-		for(var i=0;i<sequence.length;i++){
-			setInterval(function(){
-				lightUp()
-			},600);
-		}
+  startGame: function () {
+    this.round = 1
+    this.move = []
+    this.gameover = false
+    this.sequence = []
+    this.newRound()
+  },
 
-	}
+  // New round where a new move is added to the sequence
+  newRound: function () {
+    $('.roundNum').text(this.round++)
+    this.disableBoard()
+    this.sequence.push(this.randomMove())
+    this.move = this.sequence.slice()
+    this.animate(this.sequence)
+  },
 
-	function randomize(){
-		return Math.floor(Math.random()*4)+1;
-	}
+  animate: function (sequence) {
+    var i = 0
+    var interval = setInterval(() => {
+      this.lightUp(sequence[i])
+      i++
+      if (i >= sequence.length) {
+        clearInterval(interval)
+        this.humanMove()
+      }
+    }, 750)
+  },
+
+  lightUp: function (tile) {
+    $('[data-id="' + tile + '"]').addClass('selected')
+    window.setTimeout(() => {
+      $('[data-id="' + tile + '"]').removeClass('selected')
+    }, 500)
+  },
+
+  humanMove: function () {
+    var that = this
+    $('.board').on('click', function () {
+      var expected = that.move.shift()
+      var actual = $(this).data('id')
+      that.result = expected === actual
+      that.checkWin()
+    })
+  },
+
+  // Checks for winning move
+  // 1) If sequence is not done yet then it does nothing
+  // 2) If strict mode is enabled and wrong move then game over
+  // 3) If strict mode is disabled and wrong move then repeat sequence
+  // 4) If you make it through 20 rounds then you win
+  checkWin: function () {
+    if (this.move.length === 0 && this.result) {
+      this.newRound()
+    } else if (this.strict && !this.result) {
+      this.gameOver()
+    } else if (!this.result) {
+      alert('Wrong move, try again')
+      this.disableBoard()
+      this.move = this.sequence.slice()
+      this.animate(this.sequence)
+    } else if (!this.sequence.length === 20 && this.result) {
+      this.win()
+    }
+  },
+
+  win: function () {
+    alert('you win!')
+    this.startGame()
+  },
+
+  gameOver: function () {
+    alert('GG. Try again')
+    this.startGame()
+  },
+
+  disableBoard: function () {
+    $('.disable-click').off('click')
+  },
+
+  randomMove: function () {
+    return Math.round(Math.random() * 3) + 1
+  }
 }
-
-
-
-
